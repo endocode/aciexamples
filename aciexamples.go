c89fb24a5c34ea06b84bba372a207a2443222f3f
Alban Crequy <alban@endocode.com>
2015-01-29 17:05:34 +0100

    print /proc

--- aciexamples.go
+++ aciexamples.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
+func readFile(file string) {
+	fmt.Println(file + ":")
+	dat, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("Cannot read file "+file+": ", err)
+		return
+	}
+	fmt.Print(string(dat))
+}
+
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Hello! Who am I? Where am I?")
+	fmt.Println("Args: " + strings.Join(os.Args, " "))
+
+	readFile("/proc/mounts")
 }
